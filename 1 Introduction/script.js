48bb58e802f56300a08b81d1d519d361d0a3b1d3
Harsh-agrawal-2022 <harshaagrawal70717@gmail.com>
2024-10-12 20:35:11 +0530

    first commit

---
+++ 1 Introduction/script.js
@@ -0,0 +1,19 @@
+alert("Hello World");
+
+console.log("Hey I am Console...");
+console.log("Code is running....");
+console.log("Code is executed....");
+
+var a = prompt("Enter your number");
+var istrue = confirm("Are you sure you want to leave this site...")
+
+if(istrue)
+{
+    console.log("are you sure..!");
+}
+
+else{
+    console.log("you want to stay in site...");
+}
+
+console.log("Your number is " + a);
---
+++ 2 variables, data types and objects/script.js
@@ -0,0 +1,9 @@
+console.log("Hey this is javascript");
+
+var a = 5;
+var b = 6;
+var c = "Harsh";
+
+console.log(a + b + 11)
+console.log(typeof a, typeof b, typeof c)
+
